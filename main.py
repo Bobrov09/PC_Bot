49b88a819d65dbc9b8e272883c3312138a469ece
bobro <r62bobroff@yandex.ru>
2025-10-27 21:54:08 +0300

    начальный скелет бота, команда старт

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-#initial
+from aiogram import Bot, Dispatcher, types, executor
+
+TOKEN = '8355754378:AAHYVOfkDk21mEgigVAbqr9Xf_3cXhIRh7s'
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+async def on_startup(_):
+    print('Done')
+
+@dp.message_handler(commands='start')
+async def starter(message: types.Message):
+    await message.answer(text='Добро пожаловать! Я смогу подобрать комплектующие для вашего компьютера.')
+
+
+
+
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, on_startup=on_startup)
